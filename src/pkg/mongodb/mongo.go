e827b5416987fe879f11e6fc21fc89f2ece5c084
Irvan Ahadi <irvan.ahadi@tiket.com>
2021-03-07 21:42:53 +0700

    Initial commit

---
+++ src/pkg/mongodb/mongo.go
@@ -0,0 +1,15 @@
+package mongodb
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func InitClient() (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	return mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+}
